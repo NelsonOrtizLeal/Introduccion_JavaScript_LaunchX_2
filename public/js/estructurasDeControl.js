96214340d7c533da2296ca0e02460b216d72c43a
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2022-11-16 23:34:59 -0600

    Se agrego metodo prompt y confirm, y el resultado se muestra en consola

---
+++ public/js/estructurasDeControl.js
@@ -0,0 +1,7 @@
+//IF - ELSE
+
+let age = prompt("Introduce tu edad");
+let confirmation = confirm("¿Estás de acuerdon con seguir con esto?");
+
+//* console.log("Tu edad es %s y has %s la pregunta", age, confirmation);
+console.log(`Tu edad es ${age} y has ${confirmation} la pregunta`);
