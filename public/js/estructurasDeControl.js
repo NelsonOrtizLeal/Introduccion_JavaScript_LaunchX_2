0e04df01cb9a9e4d238ec8e6fada9361b9c6f359
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2022-11-16 23:53:02 -0600

    Se valida que se introdusca un valor al pedir la edad y que este sea un numero

--- public/js/estructurasDeControl.js
+++ public/js/estructurasDeControl.js
@@ -1,7 +1,16 @@
 //IF - ELSE
 
 let age = prompt("Introduce tu edad");
-let confirmation = confirm("¿Estás de acuerdon con seguir con esto?");
 
-//* console.log("Tu edad es %s y has %s la pregunta", age, confirmation);
-console.log(`Tu edad es ${age} y has ${confirmation} la pregunta`);
+if (age && Number(age)) {
+    console.log("Podemos continuar con la validaciones");
+} else {
+    console.log("Introduce tu edad");
+    location.reload();
+}
+
+
+// let confirmation = confirm("¿Estás de acuerdon con seguir con esto?");
+
+// console.log("Tu edad es %s y has %s la pregunta", age, confirmation);
+// console.log(`Tu edad es ${age} y has ${confirmation} la pregunta`);
