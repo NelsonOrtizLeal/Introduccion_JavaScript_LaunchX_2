2074779fac8066cf52697a071eb3b074f79de8d0
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2022-11-17 00:33:01 -0600

    Se agrego funcion vaildarEdad que muestra un mensaje de acuerdo a la edad introducida por el usuario

--- public/js/estructurasDeControl.js
+++ public/js/estructurasDeControl.js
@@ -15,17 +15,33 @@ function esEntero(numero) {
     return result;
 }
 
+/**
+ * Función que muestra un mensaje de acuerdo a la edad proporcionada por el usuario
+ * @param {edad} age 
+ */
+function validarEdad(age) {
+    if (confirm("¿Estas de acuerdo con seguir con esto?")) {
+        console.log("Sigue bajo tu propio riesgo");
+
+        if (age >= 18 && age <= 150) {
+            console.log("Puedes comprobar cigaritos");
+        } else if (age > 150) {
+            console.log("Saludame a Cepillin");
+        } else if (age >= 0) {
+            console.log("Tas muy chiki, vuelve en unos años");
+        } else {
+            console.log("A caray, tu no deberias existir");
+        }
+    } else {
+        console.log("Que miedosoooo... XD");
+    }
+}
+
 let age = prompt("Introduce tu edad");
 
 if (age && Number(age) && age > 0 && esEntero(age)) {
-    console.log("Podemos continuar con la validaciones");
+    validarEdad(age);
 } else {
     console.log("Introduce tu edad");
     location.reload();
 }
-
-
-// let confirmation = confirm("¿Estás de acuerdon con seguir con esto?");
-
-// console.log("Tu edad es %s y has %s la pregunta", age, confirmation);
-// console.log(`Tu edad es ${age} y has ${confirmation} la pregunta`);
